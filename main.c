74a86743a7edc21493bf6e150ef96c3071b7105e
Malone <lucas.oliveira1720@etec.sp.gov.brgit config --local user.name Malonegit commit -m primimitogit commit -m primimitogit config --local user.email lucas.oliveira1720@etec.sp.gov.br>
2023-11-01 19:44:52 -0300

    primimito

---
+++ main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main (int argc, char *argv[]){
+
+int lista[10];
+int i;
+for (i = 0; i<10; i++){
+	printf("Digite um numero para a posicao %d: ",i);
+	scanf("%d", &lista[i]);
+	lista[i] = lista[i] * 2;
+}
+printf("Valores dobrados: \n)");
+		for(i = 0; i<10;i++){
+		printf("Posicao %d: %d\n",i,lista[i]);
+		}
+
+	return 0;
+}
